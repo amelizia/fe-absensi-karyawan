c6b5fcbd1d0471b52048bdea14eafa3981dd545c
Fitri Ayu <fitriayu.cahyaningrum@gmail.com>
2021-09-19 21:38:45 +0700

    to deploy: fixing routes

---
+++ src/Pages/Components/welcomeprofile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div> 
+            <h1 className="text-xl font-semibold"> Hello, </h1>
+            <h1 className="text-lg font-semibold"> Role: </h1>
+            <h1 className="text-lg font-semibold"> Office Location: </h1>
+            </div>
+        </div>
+      
+    );
+  };
+
+export default Profile;
