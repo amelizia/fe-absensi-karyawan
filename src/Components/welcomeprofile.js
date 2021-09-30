3ddd53b5b83c89844393358c20b3a8e0afea013c
Fitri Ayu <fitriayu.cahyaningrum@gmail.com>
2021-09-30 07:25:49 +0700

    profile: fetch api

--- src/Components/welcomeprofile.js
+++ src/Components/welcomeprofile.js
@@ -1,56 +1,59 @@
 import React, { Component } from 'react';
 
-// const Profile = () => {
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      data: []
+      data: [],
+      full_name: "",
+      role_description: "",
+      office_longitude: 0,
+      office_latitude: 0,
     };
   }
 
 componentDidMount(){
     console.log('Component Mounted')
-  fetch('https://ipe8-worker-attendance-be.herokuapp.com/api/dashboard', {
-     // fetch('https://jsonplaceholder.typicode.com/users/1', {
+    fetch('https://ipe8-worker-attendance-be.herokuapp.com/api/dashboard', {
         method: 'GET',
-        // mode: 'no-cors', // no-cors, *cors, same-origin
+        mode: 'no-cors', // no-cors, *cors, same-origin
         // cache: 'default', 
         // credentials: 'same-origin', 
         // redirect: 'follow', 
         // referrerPolicy: 'same-origin', 
         headers: {
-          'Content-type': 'application/json',
+          'Content-type': 'application/json; charset=UTF-8',
           'Access-Control-Allow-Origin':'*' ,
         },
       })
-      // .then(response => response.json())
-      // .then(data => this.setState({ data }))
-      .then(data => {
-        console.log('Success:', data);
+      .then(response => response.json())
+      .then(data => {this.setState({ 
+        data,
+        full_name: data.data.full_name,
+        role_description: data.data.role_description,
+        office_longitude: data.data.office_longitude,
+        office_latitude: data.data.office_latitude,
+      });
+      console.log('Success:', data);
       })
+      // .then(data => {
+      //   console.log('Success:', data);
+      // })
       .catch((error) => {
         console.error('Error:', error);
-      });
+      })
 }
 render(){
     console.log('Render Method Called')
-    const {data} = this.state;
+    const { full_name, role_description, office_longitude, office_latitude } = this.state;
 
     return (
         <div className="content">
-                {data.map((profile)=>(
-                   <div key={profile.id}>
-                    <h1 className="2xl:text-2xl md:text-xl text-lg font-bold"> Hello, {profile.full_name}</h1>
-                    <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Role: {profile.role_description}</h1>
-                    <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Office Location: HQ Jakarta {profile.office_longitude},{profile.office_latitude} </h1>
-                   </div>
-                ))}
-            {/* <div> 
-            <h1 className="2xl:text-2xl md:text-xl text-lg font-bold"> Hello, John Doe </h1>
-            <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Role: Supervisor</h1>
-            <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Office Location: HQ Jakarta</h1>
-            </div> */}
+            <div> 
+              <h1 className="2xl:text-2xl md:text-xl text-lg font-bold"> Hello, {full_name} </h1>
+              <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Role: {role_description}</h1>
+              <h1 className="2xl:text-xl md:text-lg text-base font-semibold"> Office Location: {office_longitude}, {office_latitude}</h1>
+            </div>
         </div>
       
     )
