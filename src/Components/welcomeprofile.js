2e5f1839593cd06828d939db8179287b9880865a
Fitri Ayu <fitriayu.cahyaningrum@gmail.com>
2021-10-06 16:17:14 +0700

    fetch api with token functionality

--- src/Components/welcomeprofile.js
+++ src/Components/welcomeprofile.js
@@ -1,39 +1,27 @@
 import React, { Component, useState, useEffect } from 'react';
+import Cookies from "js-cookie";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state ={
       data: [],
-      full_name: "",
-      role_description: "",
-      office_longitude: "",
-      office_latitude: "",
+      full_name: '',
+      role_description: '',
+      office_longitude: 0,
+      office_latitude: 0,
     };
   }
-
-  // componentDidMount(){
-  //   const getProfile = async () =>{
-  //   const res = await fetch ('https://test-app-database-absensi.herokuapp.com/api/dashboard')
-  //   return await res.json()
-  //   }
-
-  //   useEffect(() => {
-  //     getProfile().then(data => {
-  //       console.log(data)
-  //     })
-  //   }, [])
-  //   return null
-  // }
 
   async componentDidMount() {
     const res = await fetch(
       "https://ipe8-worker-attendance-be.herokuapp.com/api/dashboard",
       {
         method: "GET",
-        // mode: 'no-cors',
         headers: {
-          "Content-Type": "application/json",
+          'Content-type': 'application/json; charset=UTF-8',
+          'Access-Control-Allow-Origin':'*' ,
+          Authorization: Cookies.get("token"),
         },
       }
     );
@@ -46,47 +34,7 @@ class Profile extends Component {
         office_longitude: data.data.office_longitude,
         office_latitude: data.data.office_latitude,
     });
-    
   }
-
-// componentDidMount(){
-//     console.log('Component Mounted')
-//     // fetch('https://ipe8-worker-attendance-be.herokuapp.com/api/dashboard', {
-//     fetch('https://test-app-database-absensi.herokuapp.com/api/dashboard', {
-//         method: 'GET',
-//         // mode: 'no-cors', // no-cors, *cors, same-origin
-//         // cache: 'default', 
-//         // credentials: 'same-origin', 
-//         // redirect: 'follow', 
-//         // referrerPolicy: 'same-origin', 
-//         headers: {
-//           'Content-type': 'application/json; charset=UTF-8',
-//           'Access-Control-Allow-Origin':'*' ,
-//           'Accept': 'application/json',
-//         },
-//       })
-//       // .then(response => response.json())
-//       .then(function (response) {
-//         if (!response.ok) {
-//            return response.text().then(result => Promise.reject(new Error(result)));
-//         }
-    
-//         return response.json();
-//     })
-//       .then(data => this.setState({ 
-//         data,
-//         full_name: data.data.full_name,
-//         role_description: data.data.role_description,
-//         office_longitude: data.data.office_longitude,
-//         office_latitude: data.data.office_latitude,
-//       }))
-//       // .then(data => {
-//       //   console.log('Success:', data);
-//       // })
-//       // .catch((error) => {
-//       //   console.error('Error:', error);
-//       // })
-// }
 
 render(){
     console.log('Render Method Called')
